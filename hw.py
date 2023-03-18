5e4a0f0aedbdde5c6365f17bc510f8e51cf47a02
RGGH <pisscompany@gmail.com>
2023-03-18 16:49:22 +0000

    1

---
+++ hw.py
@@ -0,0 +1,11 @@
+""" example for GitHub Actions with Sphinx """
+
+
+def hw():
+    """
+    do the hello world shizz
+
+    :return: Hello World
+    :rtype: String
+    """
+    return "hello world"
