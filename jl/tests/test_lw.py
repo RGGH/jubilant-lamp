42868836d00d27f12294b2681eeb0c8394828291
RGGH <pisscompany@gmail.com>
2023-03-20 13:01:26 +0000

    Update test_lw

--- jl/tests/test_lw.py
+++ jl/tests/test_lw.py
@@ -1,9 +1,8 @@
-import pytest
-
 """
 test lw multiply function
 """
 
+import pytest
 from lw import multiply
 
 
@@ -12,12 +11,12 @@ def test_multiply_success():
     """
     Verify 2 * 3 equals 6
     """
-    assert multiply(2,3) == 6
+    assert multiply(2, 3) == 6
 
+# Check for text input
 def test_multiply_value_err():
     """
     Verify string input fails
     """
     with pytest.raises(TypeError):
-        multiply('two','three')
-    
+        multiply("two", "three")
