9bdc23d1b627a4319882547469e732c2c616477e
RGGH <pisscompany@gmail.com>
2023-03-20 11:54:31 +0000

    Fix lw return

--- jl/lw.py
+++ jl/lw.py
@@ -6,6 +6,7 @@ Bonus file
 def multiply(x: int, y: int) -> int:
     """
     Multiplier Function
+    Calculates area
 
     :param x: length
     :type x: int
@@ -14,3 +15,4 @@ def multiply(x: int, y: int) -> int:
     :return: area
     :rtype: int
     """
+    return x * y
