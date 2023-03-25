eefb86f0e3aac562776f3e59aebe183d3f6f73e2
RGGH <pisscompany@gmail.com>
2023-03-25 11:01:44 +0000

    update comments for hw

--- jl/hw.py
+++ jl/hw.py
@@ -1,21 +1,23 @@
 """ example for GitHub Actions with Sphinx """
 
+
 def hello_world():
     """
-    do the hello world shizz
+    do the usual 'hello world'
 
     :return: Hello World
     :rtype: String
     """
     return "hello world!! :) "
 
-def greeter_msg(greeter :str)->str:
+
+def greeter_msg(*, greeter: str) -> str:
     """
     Custom greeting
 
-    :param greeter: name of person message will be from
+    :param greeter: name of person who message will be from
     :type greeter: str
     :return: a greeting from the greeter
     :rtype: str
     """
-    return (f"Hello to you from {greeter}")
+    return f"Hello to you from {greeter}"
